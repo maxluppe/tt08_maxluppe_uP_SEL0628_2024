a73fb8572a5066a972b8f61b2e115a7f3ff4d3bd
Maximiliam Luppe <maxluppe@users.noreply.github.com>
2024-06-21 15:38:22 -0300

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -24,7 +24,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_maxluppe_uP0628_24 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
