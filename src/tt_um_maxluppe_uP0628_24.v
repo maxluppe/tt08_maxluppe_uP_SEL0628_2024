8f2a24e812478b02cf132530aa3fb85bed5609b6
Maximiliam Luppe <maxluppe@users.noreply.github.com>
2024-06-21 15:30:31 -0300

    Update and rename project.v to tt_um_maxluppe_uP0628_24.v

--- src/tt_um_maxluppe_uP0628_24.v
+++ src/tt_um_maxluppe_uP0628_24.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_maxluppe_uP0628_24 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,19 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uo_out[7]  = 1'b0;
+    assign uio_oe  = 1;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, uio_in, 1'b0};
+
+    	uP_SEL0628_2024 cpu (
+            .clk(clk),
+            .clr_n(rst_n),
+            .data_in(ui_in),
+            .we(uo_out[6]),
+            .addr(uo_out[5:0]),
+            .data_out(uio_out)
+	);
 
 endmodule
